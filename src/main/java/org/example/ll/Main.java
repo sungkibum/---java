528c534f0c6216c6252aa7c4d4303e4efa765b16
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-18 16:34:11 +0900

    종료, 명언 등록, 명언번호 노출 코드 작성

--- src/main/java/org/example/ll/Main.java
+++ src/main/java/org/example/ll/Main.java
@@ -1,17 +1,29 @@
 package org.example.ll;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while (true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.print("명령) ");
+            String text = sc.nextLine();
+
+            if (text.equals("종료")) {
+                break;
+            } else if (text.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+                count++;
+                System.out.println(count + "번 명언이 등록되었습니다.");
+            }
         }
     }
 }
