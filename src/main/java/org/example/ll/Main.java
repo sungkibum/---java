fe7aaef315d64ec4d739661b0d8694578a561fc1
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-18 17:35:44 +0900

    목록 출력

--- src/main/java/org/example/ll/Main.java
+++ src/main/java/org/example/ll/Main.java
@@ -1,5 +1,7 @@
 package org.example.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 0;
+        String wiseSayingList[] = new String[10];
+        String authorList[] = new String[10];
         while (true) {
             System.out.println("== 명언 앱 ==");
             System.out.print("명령) ");
@@ -18,11 +22,17 @@ public class Main {
                 break;
             } else if (text.equals("등록")) {
                 System.out.print("명언 : ");
-                String wiseSaying = sc.nextLine();
+                wiseSayingList[count] = sc.nextLine();
                 System.out.print("작가 : ");
-                String author = sc.nextLine();
+                authorList[count] = sc.nextLine();
                 count++;
                 System.out.println(count + "번 명언이 등록되었습니다.");
+            } else if (text.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("--------------------");
+                for (int i = count; i >= 1; i--) {
+                    System.out.println(i + " / " + authorList[i - 1] + " / " + wiseSayingList[i - 1]);
+                }
             }
         }
     }
