0c156569677d3008563fe50d6936a769710765ea
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-19 17:31:38 +0900

    5단계 코드 리팩토링

--- src/main/java/org/example/ll/Main.java
+++ src/main/java/org/example/ll/Main.java
@@ -1,37 +1,53 @@
 package org.example.ll;
 
+
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static void main(String[] args) {
+
+        class WiseSaying {
+            private int id;
+            private String saying;
+            private String author;
+
+            public WiseSaying(int id, String saying, String author) {
+                this.id = id;
+                this.saying = saying;
+                this.author = author;
+            }
+
+
+        }
         Scanner sc = new Scanner(System.in);
-        int count = 0;
-        String wiseSayingList[] = new String[10];
-        String authorList[] = new String[10];
+        ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
+        int index = 1;
         while (true) {
             System.out.println("== 명언 앱 ==");
             System.out.print("명령) ");
-            String text = sc.nextLine();
+            String order = sc.nextLine();
 
-            if (text.equals("종료")) {
+            if (order.equals("종료")) {
                 break;
-            } else if (text.equals("등록")) {
+            } else if (order.equals("등록")) {
                 System.out.print("명언 : ");
-                wiseSayingList[count] = sc.nextLine();
+                String saying = sc.nextLine();
                 System.out.print("작가 : ");
-                authorList[count] = sc.nextLine();
-                count++;
-                System.out.println(count + "번 명언이 등록되었습니다.");
-            } else if (text.equals("목록")) {
+                String author = sc.nextLine();
+
+                wiseSayingList.add(new WiseSaying(index, saying, author));
+                System.out.println(index + "번 명언이 등록되었습니다.");
+                index++;
+            } else if (order.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
-                System.out.println("--------------------");
-                for (int i = count; i >= 1; i--) {
-                    System.out.println(i + " / " + authorList[i - 1] + " / " + wiseSayingList[i - 1]);
+                System.out.println("-------------------");
+                for (WiseSaying wiseSaying : wiseSayingList) {
+                    int id = wiseSaying.id;
+                    String saying = wiseSaying.saying;
+                    String author = wiseSaying.author;
+                    System.out.println(id + " / " + saying + " / " + author);
                 }
             }
         }
